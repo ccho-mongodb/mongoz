700ca25d214511e1c6ddf74041b4d8feffef6398
Chris Cho <chris.cho@10gen.com>
2019-09-19 01:01:44 -0400

    add colors

--- mongoz.py
+++ mongoz.py
@@ -3,6 +3,7 @@ from re import match
 from generate_certificate import create_self_signed_cert
 from pathlib import Path
 import os
+import colorama
 
 def load_yaml(fn):
     with open(fn) as f:
@@ -28,17 +29,25 @@ def add_nested_val(path, val, dictionary):
         dictionary[parts[0]] = val
 
 def show_prompt(questions, existing_config):
+    colorama.init(autoreset=True)
+    header_colors = colorama.Fore.GREEN
+    setting_colors = colorama.Fore.YELLOW
+    hint_colors = colorama.Fore.BLUE
+    question_colors = colorama.Fore.WHITE + colorama.Style.BRIGHT
+    default_colors = colorama.Fore.CYAN 
     responses = {}
     for q in questions:
         loopPrompt = True
         while loopPrompt:
             try:
                 os.system('clear')
-                def_val = get_nested_val(q['conf_path'], existing_config) if existing_config else q['default']
-                print("mongOz\n\n")
-                print("Setting: {}\n\n{}\n\n".format(q['conf_path'], q['hint']))
 
-                print("{} [{}]".format(q['text'], def_val))
+                def_val = get_nested_val(q['conf_path'], existing_config) if existing_config else q['default']
+                print(header_colors + "mongOz: Configuration Wizard for MongoDB server\n\n")
+                print(setting_colors + "Setting: {}\n\n".format(q['conf_path']))
+                print(hint_colors + "{}\n\n".format(q['hint']))
+                print(question_colors + "{} ".format(q['text']), end='')
+                print(default_colors + "[{}]".format(def_val))
                 answer = input() or def_val
                 
                 if 'validate' in q:
@@ -67,10 +76,10 @@ def validate_and_format_boolean(token):
     if not matches:
         raise Exception("Invalid input: {}".format(token))
 
-    if new_token in ["t", "y", "yes"]:
-        new_token = "true"
-    elif new_token in ["f", "n", "no"]:
-        new_token = "false" 
+    if new_token in ["true", "t", "y", "yes"]:
+        new_token = True
+    elif new_token in ["false", "f", "n", "no"]:
+        new_token = False 
 
     return new_token
 
@@ -86,8 +95,7 @@ def validate_and_format_port_number(token):
     return new_token
 
 if __name__ == "__main__":
-    # create certificate file
-    create_self_signed_cert(Path("cert_dir/"))
+
 
     existing_config = None
     # init
@@ -105,3 +113,8 @@ if __name__ == "__main__":
         yaml.dump(responses, outfile)
 
     print('Configuration file written to mongod.conf')
+
+    # create certificate file
+    print ('Creating security certificates...', end='')
+    create_self_signed_cert(Path("cert_dir/"))
+    print('Created!')
